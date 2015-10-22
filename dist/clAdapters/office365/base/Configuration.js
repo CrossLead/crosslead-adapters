69dfbd3929dc84a8834b1ca90b88ed6da1774c3e
Ben Southgate <bsouthga@gmail.com>
2015-10-22 15:06:12 -0400

    adapter connection test fix

--- dist/clAdapters/office365/base/Configuration.js
+++ dist/clAdapters/office365/base/Configuration.js
@@ -6,6 +6,8 @@ var _inherits = require('babel-runtime/helpers/inherits')['default'];
 
 var _classCallCheck = require('babel-runtime/helpers/class-call-check')['default'];
 
+var _Object$assign = require('babel-runtime/core-js/object/assign')['default'];
+
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
 Object.defineProperty(exports, '__esModule', {
@@ -22,16 +24,19 @@ var Office365BaseConfiguration = (function (_Configuration) {
   function Office365BaseConfiguration() {
     _classCallCheck(this, Office365BaseConfiguration);
 
-    _get(Object.getPrototypeOf(Office365BaseConfiguration.prototype), 'constructor', this).apply(this, arguments);
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _get(Object.getPrototypeOf(Office365BaseConfiguration.prototype), 'constructor', this).apply(this, args);
+    _Object$assign(this.options, { apiVersion: '1.0' });
   }
 
   return Office365BaseConfiguration;
 })(_baseConfiguration2['default']);
 
 exports['default'] = Office365BaseConfiguration;
-
-// Office 365 specific functionality here...
 ;
 module.exports = exports['default'];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNsQWRhcHRlcnMvb2ZmaWNlMzY1L2Jhc2UvQ29uZmlndXJhdGlvbi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztpQ0FBMEIsMEJBQTBCOzs7O0lBRS9CLDBCQUEwQjtZQUExQiwwQkFBMEI7O1dBQTFCLDBCQUEwQjswQkFBMUIsMEJBQTBCOzsrQkFBMUIsMEJBQTBCOzs7U0FBMUIsMEJBQTBCOzs7cUJBQTFCLDBCQUEwQjs7O0FBRTlDLENBQUMiLCJmaWxlIjoiY2xBZGFwdGVycy9vZmZpY2UzNjUvYmFzZS9Db25maWd1cmF0aW9uLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IENvbmZpZ3VyYXRpb24gZnJvbSAnLi4vLi4vYmFzZS9Db25maWd1cmF0aW9uJztcblxuZXhwb3J0IGRlZmF1bHQgY2xhc3MgT2ZmaWNlMzY1QmFzZUNvbmZpZ3VyYXRpb24gZXh0ZW5kcyBDb25maWd1cmF0aW9uIHtcbiAgLy8gT2ZmaWNlIDM2NSBzcGVjaWZpYyBmdW5jdGlvbmFsaXR5IGhlcmUuLi5cbn07XG4iXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNsQWRhcHRlcnMvb2ZmaWNlMzY1L2Jhc2UvQ29uZmlndXJhdGlvbi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7O2lDQUEwQiwwQkFBMEI7Ozs7SUFFL0IsMEJBQTBCO1lBQTFCLDBCQUEwQjs7QUFDbEMsV0FEUSwwQkFBMEIsR0FDeEI7MEJBREYsMEJBQTBCOztzQ0FDOUIsSUFBSTtBQUFKLFVBQUk7OztBQUNqQiwrQkFGaUIsMEJBQTBCLDhDQUVsQyxJQUFJLEVBQUM7QUFDZCxtQkFBYyxJQUFJLENBQUMsT0FBTyxFQUFHLEVBQUUsVUFBVSxFQUFFLEtBQUssRUFBRSxDQUFDLENBQUM7R0FDckQ7O1NBSmtCLDBCQUEwQjs7O3FCQUExQiwwQkFBMEI7QUFLOUMsQ0FBQyIsImZpbGUiOiJjbEFkYXB0ZXJzL29mZmljZTM2NS9iYXNlL0NvbmZpZ3VyYXRpb24uanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgQ29uZmlndXJhdGlvbiBmcm9tICcuLi8uLi9iYXNlL0NvbmZpZ3VyYXRpb24nO1xuXG5leHBvcnQgZGVmYXVsdCBjbGFzcyBPZmZpY2UzNjVCYXNlQ29uZmlndXJhdGlvbiBleHRlbmRzIENvbmZpZ3VyYXRpb24ge1xuICBjb25zdHJ1Y3RvciguLi5hcmdzKSB7XG4gICAgc3VwZXIoLi4uYXJncylcbiAgICBPYmplY3QuYXNzaWduKHRoaXMub3B0aW9ucywgIHsgYXBpVmVyc2lvbjogJzEuMCcgfSk7XG4gIH1cbn07XG4iXX0=
 //# sourceMappingURL=../../../clAdapters/office365/base/Configuration.js.map
