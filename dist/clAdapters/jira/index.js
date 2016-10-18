9a908042ffbde10b87c4f7883efefeb4c6b94b15
Michelle Shu <shu.michelle.w@gmail.com>
2016-10-18 15:16:15 -0400

    Fix class syntax

--- dist/clAdapters/jira/index.js
+++ dist/clAdapters/jira/index.js
@@ -65,93 +65,111 @@ var JiraAdapter = function (_Adapter) {
   (0, _createClass3.default)(JiraAdapter, [{
     key: 'init',
     value: function init() {}
+  }, {
+    key: 'makeRequest',
+    value: function makeRequest(path) {
+      var uri = _url2.default.format({
+        protocol: this.credentials.protocol || 'https',
+        hostname: this.credentials.host,
+        port: this.credentials.port,
+        pathname: 'rest/api/' + this.apiVersion + '/' + path
+      });
+
+      var authorizationString = new Buffer(this.credentials.username + ':' + this.credentials.password).toString('base64');
+
+      var options = {
+        uri: uri,
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + authorizationString
+        }
+      };
+
+      return new _promise2.default(function (resolve) {
+        (0, _request2.default)(options, function (error, response, body) {
+          var errorMessage = null;
+          var success = response && response.statusCode < 400;
+
+          if (error) {
+            success = false;
+            if (error.code === 'ECONNREFUSED') {
+              errorMessage = 'Failed to connect to JIRA adapter.';
+            }
+          }
+
+          if (response && response.statusCode === 401) {
+            success = false;
+            errorMessage = 'Failed to authorize JIRA adapter.';
+          }
+
+          resolve({
+            code: success ? 200 : 500,
+            message: errorMessage || error,
+            data: body,
+            success: success
+          });
+        });
+      });
+    }
+  }, {
+    key: 'getIssueHierarchy',
+    value: function () {
+      var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee() {
+        return _regenerator2.default.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return this.makeRequest('issue/createmeta');
+
+              case 2:
+                return _context.abrupt('return', _context.sent);
+
+              case 3:
+              case 'end':
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function getIssueHierarchy() {
+        return _ref.apply(this, arguments);
+      }
+
+      return getIssueHierarchy;
+    }()
+  }, {
+    key: 'runConnectionTest',
+    value: function () {
+      var _ref2 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2() {
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return this.makeRequest('myself');
+
+              case 2:
+                return _context2.abrupt('return', _context2.sent);
+
+              case 3:
+              case 'end':
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this);
+      }));
+
+      function runConnectionTest() {
+        return _ref2.apply(this, arguments);
+      }
+
+      return runConnectionTest;
+    }()
   }]);
   return JiraAdapter;
 }(_Adapter3.default);
 
 exports.default = JiraAdapter;
-
-
-JiraAdapter.prototype.makeRequest = function (path) {
-  var uri = _url2.default.format({
-    protocol: this.credentials.protocol || 'https',
-    hostname: this.credentials.host,
-    port: this.credentials.port,
-    pathname: 'rest/api/' + this.apiVersion + '/' + path
-  });
-
-  var authorizationString = new Buffer(this.credentials.username + ':' + this.credentials.password).toString('base64');
-
-  var options = {
-    uri: uri,
-    method: 'GET',
-    headers: {
-      'Authorization': 'Basic ' + authorizationString
-    }
-  };
-
-  return new _promise2.default(function (resolve) {
-    (0, _request2.default)(options, function (error, response, body) {
-      var errorMessage = null;
-      var success = response && response.statusCode < 400;
-
-      if (error) {
-        success = false;
-        if (error.code === 'ECONNREFUSED') {
-          errorMessage = 'Failed to connect to JIRA adapter.';
-        }
-      }
-
-      if (response && response.statusCode === 401) {
-        success = false;
-        errorMessage = 'Failed to authorize JIRA adapter.';
-      }
-
-      resolve({
-        code: success ? 200 : 500,
-        message: errorMessage || error,
-        data: body,
-        success: success
-      });
-    });
-  });
-};
-
-JiraAdapter.prototype.getIssueHierarchy = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee() {
-  return _regenerator2.default.wrap(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          _context.next = 2;
-          return this.makeRequest('issue/createmeta');
-
-        case 2:
-          return _context.abrupt('return', _context.sent);
-
-        case 3:
-        case 'end':
-          return _context.stop();
-      }
-    }
-  }, _callee, this);
-}));
-
-JiraAdapter.prototype.runConnectionTest = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2() {
-  return _regenerator2.default.wrap(function _callee2$(_context2) {
-    while (1) {
-      switch (_context2.prev = _context2.next) {
-        case 0:
-          _context2.next = 2;
-          return this.makeRequest('myself');
-
-        case 2:
-          return _context2.abrupt('return', _context2.sent);
-
-        case 3:
-        case 'end':
-          return _context2.stop();
-      }
-    }
-  }, _callee2, this);
-}));
 //# sourceMappingURL=../../clAdapters/jira/index.js.map
