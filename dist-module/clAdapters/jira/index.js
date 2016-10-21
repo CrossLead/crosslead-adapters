664cf4b6033884b20f3f72200c77fd42cb00b942
Michelle Shu <shu.michelle.w@gmail.com>
2016-10-21 11:09:23 -0400

    feat: DA-90 Add query for tasks and subtasks under an epic

--- dist-module/clAdapters/jira/index.js
+++ dist-module/clAdapters/jira/index.js
@@ -78,7 +78,7 @@ var JiraAdapter = function (_Adapter) {
   }, {
     key: 'getAllIssues',
     value: function () {
-      var _ref = _asyncToGenerator(_regeneratorRuntime.mark(function _callee(requestPath, params) {
+      var _ref = _asyncToGenerator(_regeneratorRuntime.mark(function _callee(params) {
         var requestParams, resultCount, issues, result, data;
         return _regeneratorRuntime.wrap(function _callee$(_context) {
           while (1) {
@@ -93,7 +93,7 @@ var JiraAdapter = function (_Adapter) {
 
               case 4:
                 _context.next = 6;
-                return this.makeRequest(requestPath, requestParams);
+                return this.makeRequest('search', requestParams);
 
               case 6:
                 result = _context.sent;
@@ -125,7 +125,7 @@ var JiraAdapter = function (_Adapter) {
         }, _callee, this);
       }));
 
-      function getAllIssues(_x, _x2) {
+      function getAllIssues(_x) {
         return _ref.apply(this, arguments);
       }
 
@@ -196,7 +196,7 @@ var JiraAdapter = function (_Adapter) {
             switch (_context4.prev = _context4.next) {
               case 0:
                 _context4.next = 2;
-                return this.getAllIssues('search', {
+                return this.getAllIssues({
                   jql: 'project=' + projectId + ' AND issuetype=Epic AND resolution=Unresolved'
                 });
 
@@ -211,11 +211,41 @@ var JiraAdapter = function (_Adapter) {
         }, _callee4, this);
       }));
 
-      function getUnresolvedEpicsForProject(_x3) {
+      function getUnresolvedEpicsForProject(_x2) {
         return _ref4.apply(this, arguments);
       }
 
       return getUnresolvedEpicsForProject;
+    }()
+  }, {
+    key: 'getIssuesForEpic',
+    value: function () {
+      var _ref5 = _asyncToGenerator(_regeneratorRuntime.mark(function _callee5(epicId, issueTypes) {
+        return _regeneratorRuntime.wrap(function _callee5$(_context5) {
+          while (1) {
+            switch (_context5.prev = _context5.next) {
+              case 0:
+                _context5.next = 2;
+                return this.getAllIssues({
+                  jql: '("Epic Link"=' + epicId + ' OR parent IN tempoEpicIssues(' + epicId + ')) AND issuetype IN (' + issueTypes.join(',') + ')'
+                });
+
+              case 2:
+                return _context5.abrupt('return', _context5.sent);
+
+              case 3:
+              case 'end':
+                return _context5.stop();
+            }
+          }
+        }, _callee5, this);
+      }));
+
+      function getIssuesForEpic(_x3, _x4) {
+        return _ref5.apply(this, arguments);
+      }
+
+      return getIssuesForEpic;
     }()
   }]);
 
