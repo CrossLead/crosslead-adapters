598ec36410c94e9b60ae244b88e1561f32179195
shindigy <markb@dimensiology.com>
2017-04-22 08:15:21 -0500

    added dist files for linked account types

---
+++ dist/lib/clAdapters/adapterLinkedAccountTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const adapterTypes_1 = require("./adapterTypes");
+/**
+ * Array of enums values for different user linked adapter types
+ *
+ * @array
+ */
+const AdapterLinkedAccountTypes = [
+    adapterTypes_1.default.ACTIVE_SYNC_CALENDAR
+];
+exports.default = AdapterLinkedAccountTypes;
+//# sourceMappingURL=adapterLinkedAccountTypes.js.map
