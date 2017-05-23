34429acb97d5731e4683a173f2aab537d4b324ff
Ben Southgate <bsouthga@gmail.com>
2017-05-23 16:17:57 -0400

    fix: add all dist files

---
+++ dist/clAdapters/adapterStatus.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Enumeration for adapter status. This is really only for
+ * application-specific serialization but both `crosslead-platform`
+ * and background workers will need this, so this as a
+ * common place is as good as any.
+ *
+ * @enum
+ */
+var AdapterStatus;
+(function (AdapterStatus) {
+    AdapterStatus[AdapterStatus["ACTIVE"] = 1] = "ACTIVE";
+    AdapterStatus[AdapterStatus["DELETED"] = 2] = "DELETED";
+})(AdapterStatus || (AdapterStatus = {}));
+exports.default = AdapterStatus;
+//# sourceMappingURL=adapterStatus.js.map
