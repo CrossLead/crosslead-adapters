68bafae40decde90994721444f971ea96cf282da
shindigy <markb@dimensiology.com>
2017-05-25 15:58:55 -0500

    fix: Exchange Linked Account syncs incorrect events

---
+++ dist/clAdapters/fields/types.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Enumeration for different data field types
+ *
+ * @enum
+ */
+export declare enum Types {
+    EXT_ENTITY = 1,
+    USER = 2,
+    GROUP = 3,
+    ORGANIZATION = 4,
+    RELATIONSHIP = 5,
+}
