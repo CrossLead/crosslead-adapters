bba79159f3fd4926504f7a0332b85b823463b388
shindigy <markb@dimensiology.com>
2017-05-30 13:11:15 -0500

    added dist files

---
+++ dist/clAdapters/adapterStatus.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Enumeration for adapter status. This is really only for
+ * application-specific serialization but both `crosslead-platform`
+ * and background workers will need this, so this as a
+ * common place is as good as any.
+ *
+ * @enum
+ */
+declare enum AdapterStatus {
+    ACTIVE = 1,
+    DELETED = 2,
+}
+export default AdapterStatus;
---
+++ dist/clAdapters/adapterTypes.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Enumeration for different adapter types
+ *
+ * @enum
+ */
+declare enum AdapterTypes {
+    CUSTOM = 1,
+    NETSUITE = 2,
+    CL_MOCK = 3,
+    OFFICE365 = 4,
+    GOOGLE = 5,
+    GOOGLE_CALENDAR = 6,
+    OFFICE365_CALENDAR = 7,
+    JIRA = 8,
+    SLACK = 9,
+    ACTIVE_SYNC_CALENDAR = 10,
+}
+export default AdapterTypes;
