0af94046b0e6759b0dd22007e23e1e17adbee255
Christopher Neeb <christopher.neeb@crosslead.com>
2017-06-19 10:13:07 -0400

    FIX lint

--- lib/clAdapters/google/index.ts
+++ lib/clAdapters/google/index.ts
@@ -1,6 +1,6 @@
 import GoogleMailAdapter     from './mail';
 import GoogleCalendarAdapter from './calendar';
-import GoogleOauthCalendarAdapter from './oauthCalendar'
+import GoogleOauthCalendarAdapter from './oauthCalendar';
 
 export default {
   GoogleMailAdapter,
