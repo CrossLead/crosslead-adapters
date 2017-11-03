5db9fc91dbb8a57a06916d68a146951dce00c500
Nick Wormley <nick.wormley@crosslead.com>
2017-11-03 15:35:22 -0500

    fix: Add missing file

---
+++ lib/common/types.ts
@@ -0,0 +1,12 @@
+
+export type DateRange = {
+  readonly start: Date;
+  readonly end: Date;
+};
+
+export type UserProfile = {
+  readonly email: string;
+  readonly emailAfterMapping: string;
+};
+
+
