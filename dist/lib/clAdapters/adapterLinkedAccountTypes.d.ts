63032febbd37d1e5fdfa25d5b4c64814982a1d52
Nick Wormley <nick.wormley@crosslead.com>
2018-04-08 17:24:53 -0500

    chore: Remove delted dist files

--- dist/lib/clAdapters/adapterLinkedAccountTypes.d.ts
+++
@@ -1,8 +0,0 @@
-import AdapterTypes from './adapterTypes';
-/**
- * Array of enums values for different user linked adapter types
- *
- * @array
- */
-declare const AdapterLinkedAccountTypes: AdapterTypes[];
-export default AdapterLinkedAccountTypes;
