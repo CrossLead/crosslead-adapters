793e30c68bf0658dad628703de754f8a24c79b79
Adam Houck <adam.houck@crosslead.com>
2020-06-29 19:05:13 -0400

    tweak

--- lib/clAdapters/microsoftTeams/index.ts
+++ lib/clAdapters/microsoftTeams/index.ts
@@ -4,25 +4,37 @@ import { Client } from '@microsoft/microsoft-graph-client';
 
 export type TeamsCredentials = {
     refresh_token: string;
-    client_id: string,
+    access_token: string,
+    scope: string,
+    token_type: string,
+    expires: number;
+    organizationId: string,
+    orgName: string,
+    appId: string
+
 };
 
-const scope = 'offline_access User.ReadWrite.All Group.ReadWrite.All';
-
 export default class MicrosoftTeamsAdapter extends Adapter {
+
   credentials: TeamsCredentials = {
     refresh_token: '',
-    client_id: ''
+    access_token: ''
+    scope: '',
+    token_type: 'Bearer',
+    expires: 0,
+    organizationId: '',
+    orgName: '',
+    appId: ''
+
   };
 
-  sensitiveCredentialsFields: (keyof TeamsCredentials)[] = ['credentials.refresh_token','credentials.access_token'];
+  sensitiveCredentialsFields: (keyof TeamsCredentials)[] = ['refresh_token', 'access_token'];
 
   /**
    * Rate limit api calls to once per second.
    */
   @rateLimit<any>(1000)
   callTeamsApiMethod(method: string, params: {[key: string]: string} = {}): any {
-    console.log('callTeamsApiMethod ' + method);
     let paramString = '';
     for (const p in params) {
       paramString += `${p}=${params[p]}&`;
@@ -34,33 +46,31 @@ export default class MicrosoftTeamsAdapter extends Adapter {
   }
 
   //The access token is good only for an hour 
-  async init() {
-   
-  }
+  async init() {}
 
 
   async const requestToken () {
   try {
-    let body = `${clientId}&scope=${encodeURI(scope)}&redirect_uri=${encodeURI(
-      myCurrentURL
-    )}/api/msTeams/oauthCallback&client_secret=AA-d3=391tUm/AfyaQ_]Wp.i?B3TzfX8`;
+    // let body = `${clientId}&scope=${encodeURI(scope)}&redirect_uri=${encodeURI(
+    //   myCurrentURL
+    // )}/api/msTeams/oauthCallback&client_secret=AA-d3=391tUm/AfyaQ_]Wp.i?B3TzfX8`;
 
-    if (isRefresh) {
-      body = body + `&refresh_token=${accessCode}&grant_type=refresh_token`;
-    } else {
-      body = body + `&code=${accessCode}&grant_type=authorization_code`;
-      // body = body + `&grant_type=client_credentials`;
-    }
+    // if (isRefresh) {
+    //   body = body + `&refresh_token=${accessCode}&grant_type=refresh_token`;
+    // } else {
+    //   body = body + `&code=${accessCode}&grant_type=authorization_code`;
+    //   // body = body + `&grant_type=client_credentials`;
+    // }
 
-    const paramString = `/${tenant}/oauth2/v2.0/token`;
-    const response = await fetch(`${MICROSOFT_LOGIN}${paramString}`, {
-      method: 'POST',
-      body,
-    });
+    // const paramString = `/${tenant}/oauth2/v2.0/token`;
+    // const response = await fetch(`${MICROSOFT_LOGIN}${paramString}`, {
+    //   method: 'POST',
+    //   body,
+    // });
 
-    return (await response.json()) as TokenSuccessResponse;
+    // return (await response.json()) as TokenSuccessResponse;
   } catch (err) {
-    logger.error(err);
+    // logger.error(err);
     throw err;
   }
 };
