708efd650bd2e53d29562a3b92f8955544458704
Adam Houck <adam.houck@crosslead.com>
2020-06-29 18:47:12 -0400

    Microsoft Teams: Adding correct credentials

--- lib/clAdapters/microsoftTeams/index.ts
+++ lib/clAdapters/microsoftTeams/index.ts
@@ -1,50 +1,69 @@
 import Adapter from '../base/Adapter';
-import * as request from 'request';
 import rateLimit from '../../utils/rate-limit';
+import { Client } from '@microsoft/microsoft-graph-client';
 
 export type TeamsCredentials = {
-    access_token: string;
-    scope: string;
+    refresh_token: string;
+    client_id: string,
 };
+
+const scope = 'offline_access User.ReadWrite.All Group.ReadWrite.All';
 
 export default class MicrosoftTeamsAdapter extends Adapter {
   credentials: TeamsCredentials = {
-    access_token: '',
-    scope: ''
+    refresh_token: '',
+    client_id: ''
   };
-  sensitiveCredentialsFields: (keyof TeamsCredentials)[] = ['access_token'];
 
-  static baseApiUrl = 'https://slack.com/api';
+  sensitiveCredentialsFields: (keyof TeamsCredentials)[] = ['credentials.refresh_token','credentials.access_token'];
 
   /**
-   * Rate limit (at prototype level) slack api calls to once per second.
+   * Rate limit api calls to once per second.
    */
   @rateLimit<any>(1000)
-  static callTeamsApiMethod(method: string, params: {[key: string]: string} = {}): any {
-    console.log('callTeamsApiMethod');
+  callTeamsApiMethod(method: string, params: {[key: string]: string} = {}): any {
+    console.log('callTeamsApiMethod ' + method);
     let paramString = '';
     for (const p in params) {
       paramString += `${p}=${params[p]}&`;
     }
 
     return new Promise((resolve, reject) => {
-      request.get(
-        `${this.baseApiUrl}/${method}?${paramString}`,
-        (err, resp, body) => {
-          if (!err && resp.statusCode === 200) {
-            resolve(JSON.parse(body));
-          } else {
-            reject(err);
-          }
-        }
-      );
+
     });
   }
 
-  // null init function...
+  //The access token is good only for an hour 
   async init() {
-    console.log('init MSTeams Adapter');
+   
   }
+
+
+  async const requestToken () {
+  try {
+    let body = `${clientId}&scope=${encodeURI(scope)}&redirect_uri=${encodeURI(
+      myCurrentURL
+    )}/api/msTeams/oauthCallback&client_secret=AA-d3=391tUm/AfyaQ_]Wp.i?B3TzfX8`;
+
+    if (isRefresh) {
+      body = body + `&refresh_token=${accessCode}&grant_type=refresh_token`;
+    } else {
+      body = body + `&code=${accessCode}&grant_type=authorization_code`;
+      // body = body + `&grant_type=client_credentials`;
+    }
+
+    const paramString = `/${tenant}/oauth2/v2.0/token`;
+    const response = await fetch(`${MICROSOFT_LOGIN}${paramString}`, {
+      method: 'POST',
+      body,
+    });
+
+    return (await response.json()) as TokenSuccessResponse;
+  } catch (err) {
+    logger.error(err);
+    throw err;
+  }
+};
 
   async getFieldData() {
     throw new Error('Microsoft Teams adapters currently do not support `getFieldData()`');
